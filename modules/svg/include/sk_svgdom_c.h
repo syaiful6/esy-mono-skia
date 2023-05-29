e9a155ebbae8f99cfeecbac2ab7de5998520a6c5
syaiful6 <syai.bahri6@gmail.com>
2023-05-29 16:52:24 +0700

    fix header file for sk_svgdom_c

--- modules/svg/include/sk_svgdom_c.h
+++ modules/svg/include/sk_svgdom_c.h
@@ -8,7 +8,7 @@ SK_C_API void sk_svgdom_render(sk_svgdom_t *svgdom, sk_canvas_t *canvas);
 SK_C_API void sk_svgdom_set_container_size(sk_svgdom_t *svgdom, float width, float height);
 SK_C_API float sk_svgdom_get_container_width(sk_svgdom_t *svgdom);
 SK_C_API float sk_svgdom_get_container_height(sk_svgdom_t *svgdom);
-SK_C_API sk_svgdom_t* sk_create_from_stream(sk_stream_t *stream);
+SK_C_API sk_svgdom_t* sk_svgdom_create_from_stream(sk_stream_t *stream);
 SK_C_API void sk_svgdom_ref(const sk_svgdom_t *svg);
 SK_C_API void sk_svgdom_unref(const sk_svgdom_t *svg);
 
